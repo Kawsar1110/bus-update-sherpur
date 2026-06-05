5e8e476b8a70be45ae56aafcccc643d268e9c352
kawsar1110 <mdkawsar15-6951@diu.edu.bd>
2026-06-05 11:23:28 +0600

    new icon added

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      includeAssets: ['busicon.png'],
       manifest: {
         name: 'শেরপুর বাস সময়সূচী',
         short_name: 'শেরপুর বাস',
